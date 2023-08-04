3a0b1557fa8ca3a59412537384d581564cd586b6
Matiss Janis Aboltins <matiss@mja.lv>
2023-08-04 18:52:05 +0100

    Add missing useEffect

--- packages/desktop-client/src/components/reports/Overview.js
+++ packages/desktop-client/src/components/reports/Overview.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { VictoryBar, VictoryGroup, VictoryVoronoiContainer } from 'victory';
